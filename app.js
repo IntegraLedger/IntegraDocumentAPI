7f9d070b5d8d02fd2c2e030cf77293b3ccecafb5
alexey <alexeybreusov5@gmail.com>
2019-12-11 18:46:17 +0200

    Add logo into QRCode of PDF file Add QRVerify page with mongo

--- app.js
+++ app.js
@@ -16,8 +16,34 @@ const PizZip = require('pizzip')
 const Docxtemplater = require('docxtemplater')
 const libre = require('libreoffice-convert')
 const libreConvertAsync = promisify(libre.convert)
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const QRSchema = new Schema({
+  guid: String,
+  hash: String,
+})
+const QRModel = mongoose.model('QR', QRSchema);
+const uuidv1 = require('uuid/v1')
+const moment = require('moment')
 const app = express()
 app.use(cors())
+app.use(express.static('public'))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs');
+
+// Connect to mongodb
+const uri = 'mongodb+srv://dbIntegra:password22@cluster0-qqiiz.azure.mongodb.net/integra?retryWrites=true&w=majority';
+mongoose.connect(uri, {
+  dbName: 'dbIntegra',
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log('DB connected.')
+  })
+  .catch(err => {
+    console.log('Error: Unable to connect to db. ' + err)
+  })
 
 app.post('/pdf', upload.single('file'), async (req, res) => {
   try {
@@ -41,7 +67,8 @@ app.post('/pdf', upload.single('file'), async (req, res) => {
     fillForm(writer, meta)
 
     // Add QR Code into first page
-    await QRCode.toFile('qr.png', 'integra')
+    const guid = uuidv1()
+    await QRCode.toFile('qr.png', guid)
     const pageBox = reader.parsePage(0).getMediaBox()
     const pageWidth = pageBox[2] - pageBox[0]
     const pageHeight = pageBox[3] - pageBox[1]
@@ -54,9 +81,16 @@ app.post('/pdf', upload.single('file'), async (req, res) => {
         fit: 'always'
       }
     })
+    ctx.drawImage(pageWidth - 65, pageHeight - 65, 'integra-qr.png', {
+      transformation: {
+        width: 30,
+        height: 30,
+        fit: 'always'
+      }
+    })
     pageModifier.endContext().writePage()
     pageModifier
-      .attachURLLinktoCurrentPage('http://google.com', pageWidth - 100, pageHeight, pageWidth, pageHeight - 100)
+      .attachURLLinktoCurrentPage('https://integraapi.azurewebsites.net/QRVerify/' + guid, pageWidth - 100, pageHeight, pageWidth, pageHeight - 100)
       .endContext().writePage()
     writer.end()
 
@@ -84,6 +118,12 @@ app.post('/pdf', upload.single('file'), async (req, res) => {
     })
     // console.log(encryptedData)
     // console.log(await response.json())
+
+    // Store GUID and Hash to MongoDB
+    const instance = new QRModel()
+    instance.guid = guid
+    instance.hash = encryptedData
+    instance.save()
     
     // Attach file name to response header
     res.setHeader('Access-Control-Expose-Headers', 'file-name')
@@ -128,7 +168,8 @@ app.post('/doc', upload.single('file'), async (req, res) => {
     infoDictionary.addAdditionalInfoEntry('infoJSON', JSON.stringify(meta))
 
     // Add QR Code into first page
-    await QRCode.toFile('qr.png', 'integra')
+    const guid = uuidv1()
+    await QRCode.toFile('qr.png', guid)
     const pageBox = reader.parsePage(0).getMediaBox()
     const pageWidth = pageBox[2] - pageBox[0]
     const pageHeight = pageBox[3] - pageBox[1]
@@ -141,9 +182,16 @@ app.post('/doc', upload.single('file'), async (req, res) => {
         fit: 'always'
       }
     })
+    ctx.drawImage(pageWidth - 65, pageHeight - 65, 'integra-qr.png', {
+      transformation: {
+        width: 30,
+        height: 30,
+        fit: 'always'
+      }
+    })
     pageModifier.endContext().writePage()
     pageModifier
-      .attachURLLinktoCurrentPage('http://google.com', pageWidth - 100, pageHeight, pageWidth, pageHeight - 100)
+      .attachURLLinktoCurrentPage('https://integraapi.azurewebsites.net/QRVerify/' + guid, pageWidth - 100, pageHeight, pageWidth, pageHeight - 100)
       .endContext().writePage()
     writer.end()
     
@@ -171,7 +219,13 @@ app.post('/doc', upload.single('file'), async (req, res) => {
     })
     // console.log(encryptedData)
     // console.log(await response.json())
-    
+
+    // Store GUID and Hash to MongoDB
+    const instance = new QRModel()
+    instance.guid = guid
+    instance.hash = encryptedData
+    instance.save()
+
     // Attach file name to response header
     res.setHeader('Access-Control-Expose-Headers', 'file-name')
     res.setHeader('file-name', fileName)
@@ -179,6 +233,35 @@ app.post('/doc', upload.single('file'), async (req, res) => {
     res.download('modified/' + fileName, fileName)
   } catch (err) {
     console.log('err')
+    res.send(err)
+  }
+})
+
+app.get('/QRVerify/:guid', async (req, res) => {
+  try {
+    const qrData = await QRModel.findOne({ guid: req.params.guid }).exec()
+    if (qrData) {
+      const response = await fetch('https://integraledger.azure-api.net/api/v1.4/valueexists/' + qrData.hash, {
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+          'Ocp-Apim-Subscription-Key': 'd1097c4c28ba4b09accd006d1162ad78'
+        },
+      })
+      const responseJson = await response.json()
+      if (responseJson.exists) {
+        res.render('success', {
+          guid: responseJson.data[0].Record.identityId,
+          hash: responseJson.data[0].Record.value,
+          createdAt: moment(responseJson.data[0].Record.creationDate).format('LLL')
+        })
+      } else {
+        res.render('failure')
+      }
+    } else {
+      res.render('failure')
+    }
+  } catch (err) {
     res.send(err)
   }
 })
